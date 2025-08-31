2fe5ea689c4650f0b5a3770b0ba0135c7efb62b6
hedilarbi <hedi.larbi@esprit.tn>
2025-08-31 21:27:07 +0100

    wfewfwe

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,10 +5,8 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { MdDashboard } from "react-icons/md";
-import { signOut } from "firebase/auth";
-import { auth } from "@/lib/firebase";
 
-import { FaUsers, FaTicket } from "react-icons/fa6";
+import { FaUsers } from "react-icons/fa6";
 
 // Main navigation items
 const navItems = [
